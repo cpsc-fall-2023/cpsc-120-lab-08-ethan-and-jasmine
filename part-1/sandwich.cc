fd18770862b755186f6e9e5667b9b16e0f66793c
Tirtorahardjo <etirtorahardjo@cs300u-1c.acad.fullerton.edu>
2023-10-30 18:17:36 -0700

    changes

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -9,7 +9,7 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-   if (arguments.size() != 4) {
+  if (arguments.size() != 4) {
     std::cout << "error: you must supply three arguments\n";
     return 1;
   }
